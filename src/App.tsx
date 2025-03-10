d3cd2d591af4aaa3f023658f6537bfa4a80b4a66
Kanish Yathra Raj <109946890+KanishYathraRaj@users.noreply.github.com>
2025-03-10 16:58:47 +0530

    Copy src/App.tsx from typebattle-academy

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from './context/ThemeContext';
+import { TestProvider } from './context/TestContext';
+import Index from "./pages/Index";
+import Test from "./pages/Test";
+import Results from "./pages/Results";
+import Settings from "./pages/Settings";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+      <TestProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/test" element={<Test />} />
+              <Route path="/results" element={<Results />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </BrowserRouter>
+        </TooltipProvider>
+      </TestProvider>
+    </ThemeProvider>
+  </QueryClientProvider>
+);
+
+export default App;
